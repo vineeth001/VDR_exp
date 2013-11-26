466c04e05f4dda7f9cfdcff6770a0f3c5b8fccc8
Vineeth <twinu89@gmail.com>
2013-11-26 13:25:34 +0800

    Delete script.sh

--- VDR_exp/script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-LIMIT=0.06
-> pir_range.csv
-for a in $(seq 0.01 0.0001 $LIMIT)   # Double parentheses, and "LIMIT" with no "$".
-do
-    echo $a >> pir_range.csv
-done                           # A construct borrowed from 'ksh93'.
-
